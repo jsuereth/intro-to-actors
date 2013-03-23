73d235d26709ef68447f4827b032b08216c399ee
Josh Suereth <joshua.suereth@gmail.com>
2013-03-22 22:31:57 -0400

    Adding basic Berkeley DB support for embedded local-node store.

--- build.sbt
+++ build.sbt
@@ -4,11 +4,14 @@ organization := "com.jsuereth"
 
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
+resolvers += "Oracle repo" at "http://download.oracle.com/maven"
+
 scalaVersion := "2.10.1"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.1.0",
-  "org.specs2" %% "specs2" % "1.14" % "test"
+  "org.specs2" %% "specs2" % "1.14" % "test",
+  "com.sleepycat" % "je" % "3.3.75"
 )
 
 scalacOptions ++= Seq("-feature", "-deprecation", "-optimise")
---
+++ src/main/scala/data/db/bindings.scala
@@ -0,0 +1,116 @@
+package data
+package db
+
+import com.sleepycat.je.{
+  DatabaseEntry,
+  Database
+}
+import com.sleepycat.bind.tuple.{
+  TupleBinding,
+  TupleOutput,
+  TupleInput
+}
+
+// TODO - Clean this up *A LOT*
+object Db {
+  import com.sleepycat.je._
+  private var env: Option[Environment] = None
+  private var hotelDb: Option[DatabaseHelper[String, Hotel]] = None
+  def open(): Unit = {
+    val dbDir = new java.io.File("target/db")
+    dbDir.mkdirs()
+    val envConfig = new EnvironmentConfig()
+    envConfig setAllowCreate true
+    env = Some(new Environment(dbDir, envConfig));
+
+    // Open the database. Create it if it does not already exist.
+    val dbConfig = new DatabaseConfig()
+    dbConfig setAllowCreate true
+    hotelDb = env map (_.openDatabase(null, "hotelDb", dbConfig)) map (db => new DatabaseHelper(db, HotelSerializer)) 
+  }
+  def hotel = hotelDb.get
+  def close(): Unit = {
+    env foreach (_.close)
+    env = None
+    hotelDb foreach (_.close)
+    hotelDb = None
+  }
+}
+
+class DatabaseHelper[Key, Value](db: Database, serializer: DbSerializer[Key,Value]) {
+  import com.sleepycat.je._
+  def read(): Seq[Value] = {
+    val cursor = db.openCursor(null, null)
+    val results = collection.mutable.ArrayBuffer.empty[Value]
+    try {
+      val key = new DatabaseEntry
+      val value = new DatabaseEntry
+      def read(): Unit = cursor.getNext(key, value, LockMode.DEFAULT) match {
+        case OperationStatus.SUCCESS =>
+          results.append(serializer.deserialize(key, value))
+          read()
+        case _ => ()
+      }
+      read()
+    } finally cursor.close()
+    results.toSeq
+  }
+  def get(id: Key): Option[Value] = {
+    val key = serializer serializeKey id
+    val data = new DatabaseEntry
+    db.get(null, key, data, LockMode.DEFAULT) match {
+      case OperationStatus.SUCCESS => Some(serializer.deserialize(key, data))
+      case _ => None
+    }
+  }
+  def put(value: Value): Unit = {
+    val (key, data) = serializer serialize value
+    db.put(null, key, data)
+  }
+  def close(): Unit = db.close()
+}
+
+trait DbSerializer[Key, Value] {
+  /** Serailize a value into a key-value pair. */
+  def serialize(value: Value): (DatabaseEntry, DatabaseEntry)
+  def deserialize(key: DatabaseEntry, value: DatabaseEntry): Value
+  def serializeKey(key: Key): DatabaseEntry
+}
+
+abstract class StringKeySerializer[Value] extends DbSerializer[String, Value] {
+  private val stringBinding = TupleBinding.getPrimitiveBinding(classOf[String])
+  final def serializeKey(key: String): DatabaseEntry = {
+    val entry = new DatabaseEntry
+    stringBinding.objectToEntry(key, entry)
+    entry
+  }
+}
+/** A serializer interface for hotels. */
+object HotelSerializer extends StringKeySerializer[Hotel] {
+  def serialize(hotel: Hotel): (DatabaseEntry, DatabaseEntry) = {
+    val entry = new DatabaseEntry
+    HotelBinding.objectToEntry(hotel, entry)
+    serializeKey(hotel.id) -> entry
+  }
+  def deserialize(key: DatabaseEntry, value: DatabaseEntry): Hotel =
+    HotelBinding.entryToObject(value)
+  /** Helper object for going to/from DatabaseEntry. */  
+  private object HotelBinding extends TupleBinding[Hotel] {
+    def objectToEntry(hotel: Hotel, to: TupleOutput): Unit = {
+        to writeString hotel.id
+        to writeString hotel.name
+        to writeString hotel.description
+        to writeString hotel.location.address
+        to writeString hotel.location.city
+        to writeString hotel.location.country
+    }
+    def entryToObject(ti: TupleInput): Hotel = {
+        val id = ti.readString
+        val name = ti.readString
+        val description = ti.readString
+        val location = 
+          Location(ti.readString, ti.readString, ti.readString)
+        Hotel(id,name,description, location)
+    }
+} 
+}
